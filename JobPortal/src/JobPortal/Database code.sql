3ccc9df06ef8ffd25a88f96e9e69a4ea247766d1
up834069 <36628804+up834069@users.noreply.github.com>
2018-02-26 13:23:06 +0000

    Updated and added table
    Added the registration table that we spoke about last session and also changed a few rows in the code as it was incorrect

--- JobPortal/src/JobPortal/Database code.sql
+++ JobPortal/src/JobPortal/Database code.sql
@@ -3,12 +3,12 @@ CREATE DATABASE JobPortal;
 
 CREATE TABLE JobSearch (
 JobID int AUTO_INCREMENT not null,
-JobTitle (100) not null,
-JobType varchar (250) not null,
+JobTitle varchar (100) not null,
+JobType varchar (80) not null,
 Location varchar (50) not null,
-JobDescription (250) not null,
-Sector (60) not null,
-CompName (150) not null,
+JobDescription varchar (250) not null,
+Sector varchar(60) not null,
+CompName varchar(150) not null,
 Salary DEC(8,2) not null DEFAULT 0,
 ApplyByDate date not null,
 PRIMARY KEY (JobID)
@@ -18,5 +18,18 @@ CREATE TABLE ApplyForJob (
 JobID int,
 JobApplied ENUM ('Yes' , 'No'),
 FOREIGN KEY (JobID) REFERENCES JobSearch(JobID)
+);
+
+CREATE TABLE Registration (
+RegID int AUTO_INCREMENT not null,
+fName varchar (50) not null,
+lName varchar (50) not null,
+email varchar (200) not null,
+Pnumber int (11) not null,
+DOB date not null,
+PreLocation varchar (50) null,
+PreJobRole varchar (100) null,
+PreJobType varchar (80) null,
+PRIMARY KEY (RegID)
 );
 
